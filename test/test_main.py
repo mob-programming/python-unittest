d9a8f9c264ca550a7ef9336d867ffee30f88c84e
Mob Programmer <48465586+mob-programmer@users.noreply.github.com>
2019-11-12 19:14:13 -0800

    first passing test

--- test/test_main.py
+++ test/test_main.py
@@ -1,12 +1,10 @@
 from unittest import TestCase
 
-from src.project_main import ProjectMain
+from src.project_main import Gameboard
+
 
 
 class TestMain(TestCase):
 
-    def test_when_ProjectMain_is_made_get_greeting_returns_default_greeting(self) -> None:
-        main = ProjectMain()
-        expected = f'Hello user!'
-        actual = main.get_greeting()
-        self.assertEqual(expected, actual, 'Not the greeting I was expecting')
+    def test_gameboard_is_empty_on_initialization(self):
+        gameboard = Gameboard()
