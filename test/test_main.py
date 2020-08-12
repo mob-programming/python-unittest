dea7adbb48e7a4ab6938e4e2ff6b73e99a93f1a2
Nazee <none@right.now>
2020-08-11 18:34:15 -0700

    added space
    Co-Authored-By: mob-programmer <48465586+mob-programmer@users.noreply.github.com>
    Co-Authored-By: Matt Plavcan <2327801+mplavcan@users.noreply.github.com>

--- test/test_main.py
+++ test/test_main.py
@@ -7,6 +7,7 @@ class TestMain(TestCase):
 
     def test_when_ProjectMain_is_made_get_greeting_returns_default_greeting(self) -> None:
         main = ProjectMain()
+        
         expected = f'Hello user!'
         actual = main.get_greeting()
         self.assertEqual(expected, actual, 'Not the greeting I was expecting')
