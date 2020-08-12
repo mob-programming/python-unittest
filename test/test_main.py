505512ff6db2059303d505c300e3a8475a611e98
Matthew Plavcan <mwplavcan@gmail.com>
2020-08-11 19:07:16 -0700

    Added first Game Of Life rule as commented pseudocode

--- test/test_main.py
+++ test/test_main.py
@@ -6,6 +6,10 @@ from src.project_main import ProjectMain
 class TestMain(TestCase):
 
     def test_make_a_game(self) -> None:
+        #Create a game of Life board
+        # set (1,1) to be alive
+        # dead next cycle
+
         main = ProjectMain()
 
         expected = f'Hello user!'
